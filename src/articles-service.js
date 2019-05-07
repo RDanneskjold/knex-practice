e1435d5eb56be3f66dfffb46feb455d7ec23e0ba
Thompson Galetovic <200conceptz@gmail.com>
2019-05-07 17:39:46 -0400

    sorry

--- src/articles-service.js
+++ src/articles-service.js
@@ -7,6 +7,22 @@ const ArticlesService = {
             .insert(newArticle)
             .into('blogful_articles')
             .returning('*')
+            .then(rows => {
+                return rows[0]
+            })
+    },
+    getById(knex, id) {
+        return knex.from('blogful_articles').select('*').where('id', id).first()
+    },
+    deleteArticle(knex, id) {
+        return knex('blogful_articles')
+            .where({ id })
+            .delete()
+    },
+    updateArticle(knex, id, newArticleFields) {
+        return knex('blogful_articles')
+            .where({ id })
+            .update(newArticleFields)
     },
 }
 
